25a7952547e1c89f938138f309bc240512e63e16
cyrilvincent <contact@cyrilvincent.com>
2018-07-12 13:40:03 +0200

    Array TP 1/2

--- src/www/js/mylibrary.js
+++ src/www/js/mylibrary.js
@@ -3,19 +3,32 @@ function circleArea(rayon) {
 }
 
 function display(tab) {
-
+    for(var i=0;i<tab.length;i++) {
+        document.write(tab[i]+" ");
+    }
+    document.write("<br/>");
 }
 
 function sum(tab) {
-
+    var res = 0;
+    for(var i=0;i<tab.length;i++) {
+        res += tab[i];
+    }
+    return res;
 }
 
 function max(tab) {
-
+    var max=tab[0];
+    for(var i =1;i<tab.length;i++) {
+        if(tab[i]>max) {
+            max = tab[i];
+        }
+    }
+    return max;
 }
 
 function avg(tab) {
-
+    return sum(tab)/tab.length;
 }
 
 function isPrimeNumber(i) {
