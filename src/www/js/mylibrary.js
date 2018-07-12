b7fc286b9939b85d932a9e167887e77447bdc304
cyrilvincent <contact@cyrilvincent.com>
2018-07-12 11:59:39 +0200

    mylibrary.js

---
+++ src/www/js/mylibrary.js
@@ -0,0 +1,27 @@
+function circleArea(rayon) {
+    return Math.PI * rayon * rayon;
+}
+
+function display(tab) {
+
+}
+
+function sum(tab) {
+
+}
+
+function max(tab) {
+
+}
+
+function avg(tab) {
+
+}
+
+function isPrimeNumber(i) {
+
+}
+
+function getPrimeNumbers(tab) { // => retourne un tableau de nombre premiers
+    // tab.push <=> tab.add de Java
+}
