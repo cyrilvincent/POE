338afd71fa7eb870281cddcae94cb1b0fe19b780
cyrilvincent <contact@cyrilvincent.com>
2018-08-27 15:22:38 +0200

    JAX-RS Web.xml change

--- src/main/java/com/m2i/poe/rest/HelloWorldRest.java
+++ src/main/java/com/m2i/poe/rest/HelloWorldRest.java
@@ -1,5 +1,7 @@
 package com.m2i.poe.rest;
 
+import com.m2i.poe.media.Book;
+
 import javax.ws.rs.core.*;
 
 import javax.ws.rs.*;
@@ -12,5 +14,16 @@ public class HelloWorldRest {
     @Produces(MediaType.TEXT_PLAIN)
     public String getHello() {
         return "Hello World!";
+    }
+
+    @GET
+    @Path("/book")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Book getBook() {
+        Book b = new Book();
+        b.setTitle("JSON");
+        b.setId(12);
+        b.setPrice(99);
+        return b;
     }
 }
