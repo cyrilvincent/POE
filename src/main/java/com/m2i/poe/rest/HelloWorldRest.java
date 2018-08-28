552a5fee0a4d19ccb8c7fbb57b23ff155103ccf6
cyrilvincent <contact@cyrilvincent.com>
2018-08-28 11:55:19 +0200

    JPQL

--- src/main/java/com/m2i/poe/rest/HelloWorldRest.java
+++ src/main/java/com/m2i/poe/rest/HelloWorldRest.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.ws.rs.core.*;
 
 import javax.ws.rs.*;
+import java.util.List;
 
 @Path("/hello")
 public class HelloWorldRest {
@@ -43,5 +44,23 @@ public class HelloWorldRest {
         EntityManager em = EntityManagerFactorySingleton.getEntityManager();
         Book b = em.find(Book.class,id);
         return b;
+    }
+
+    @GET
+    @Path("/book/price/{price}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Book> getBook(@PathParam("price") double price) {
+        EntityManager em = EntityManagerFactorySingleton.getEntityManager();
+        List<Book> l = em.createQuery("select b from Book b where b.price <= "+ price).getResultList();
+        return l;
+    }
+
+    @GET
+    @Path("/book/title/{title}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Book> getBook(@PathParam("title") String title) {
+        EntityManager em = EntityManagerFactorySingleton.getEntityManager();
+        List<Book> l = em.createQuery("select b from Book b where upper(b.title) like '%"+ title.toUpperCase() + "%'").getResultList();
+        return l;
     }
 }
