3e01f6ba350e66632f29c033893163d2e8787436
cyrilvincent <contact@cyrilvincent.com>
2018-08-28 10:37:49 +0200

    JAX-RS example

--- src/main/java/com/m2i/poe/rest/HelloWorldRest.java
+++ src/main/java/com/m2i/poe/rest/HelloWorldRest.java
@@ -19,22 +19,29 @@ public class HelloWorldRest {
     }
 
     @GET
-    @Path("/mock")
+    @Path("/param/{s}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getHello(@PathParam("s") String s) {
+        return "Hello "+s;
+    }
+
+    @GET
+    @Path("/mock/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Book getMock() {
+    public Book getMock(@PathParam("id") int id) {
         Book b = new Book();
         b.setTitle("JSON");
-        b.setId(12);
+        b.setId(id);
         b.setPrice(99);
         return b;
     }
 
     @GET
-    @Path("/book")
+    @Path("/book/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Book getBook() {
+    public Book getBook(@PathParam("id") int id) {
         EntityManager em = EntityManagerFactorySingleton.getEntityManager();
-        Book b = em.find(Book.class,1);
+        Book b = em.find(Book.class,id);
         return b;
     }
 }
