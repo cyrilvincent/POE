dd9dd8539d82345db7e2ad3f2c4edd8011fc48d7
cyrilvincent <contact@cyrilvincent.com>
2018-08-28 09:44:01 +0200

    JPA Console

--- src/main/java/com/m2i/poe/rest/HelloWorldRest.java
+++ src/main/java/com/m2i/poe/rest/HelloWorldRest.java
@@ -1,7 +1,9 @@
 package com.m2i.poe.rest;
 
 import com.m2i.poe.media.Book;
+import com.m2i.poe.media.EntityManagerFactorySingleton;
 
+import javax.persistence.EntityManager;
 import javax.ws.rs.core.*;
 
 import javax.ws.rs.*;
@@ -17,13 +19,22 @@ public class HelloWorldRest {
     }
 
     @GET
-    @Path("/book")
+    @Path("/mock")
     @Produces(MediaType.APPLICATION_JSON)
-    public Book getBook() {
+    public Book getMock() {
         Book b = new Book();
         b.setTitle("JSON");
         b.setId(12);
         b.setPrice(99);
+        return b;
+    }
+
+    @GET
+    @Path("/book")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Book getBook() {
+        EntityManager em = EntityManagerFactorySingleton.getEntityManager();
+        Book b = em.find(Book.class,1);
         return b;
     }
 }
