0a166b008ea41eaa0a9dabb3524727d70ccce5ae
cyrilvincent <contact@cyrilvincent.com>
2018-08-30 09:09:23 +0200

    JUnit

---
+++ src/test/java/MediaTests.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class MediaTests {
+
+    @Test
+    public void firstTest() {
+        int i = 1;
+        i = i + 1;
+        assertEquals(2,i);
+    }
+}
