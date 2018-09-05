4cb2066d06e0a0260ea9059abdca190a6afe388d
cyrilvincent <contact@cyrilvincent.com>
2018-09-05 15:39:25 +0200

    Angular 14

--- src/www/toh/src/ts/react.ts
+++ src/www/toh/src/ts/react.ts
@@ -1,5 +1,8 @@
-var rx = require("rxjs");
-var op = require('rxjs/operators')
+//var rx = require("rxjs");
+//var op = require('rxjs/operators'); //In ts-node
+import * as rx from "rxjs";
+import * as op from 'rxjs/operators'; //In ng
+
 const myObservable = rx.of(42);
 myObservable.subscribe((value) => { console.log(value); });
 
