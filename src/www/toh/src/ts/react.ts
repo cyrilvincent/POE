b3c0c4131df44671d1caf115074e2c6a8259820f
cyrilvincent <contact@cyrilvincent.com>
2018-09-05 11:44:28 +0200

    Angular 13

---
+++ src/www/toh/src/ts/react.ts
@@ -0,0 +1,30 @@
+var rx = require("rxjs");
+var op = require('rxjs/operators')
+const myObservable = rx.of(42);
+myObservable.subscribe((value) => { console.log(value); });
+
+const o = rx.of(42);
+o.pipe(
+    op.tap((value) => console.log('Avant : ' + value)),
+    op.map((value: number) => value + 1),
+    op.tap((value) => console.log('Après : ' + value)),
+).subscribe((value: number) => {
+    console.log(value);  // 43
+});
+
+var tab = [1,2,3];
+const otab = rx.from(tab);
+otab.pipe(
+    op.map((x:number) => x * 10), 
+).subscribe((x : number) => {
+    console.log(x);
+})
+
+var tab2 = [1,2,3,4];
+const otab2 = rx.from(tab2);
+otab2.pipe(
+    op.filter((x:number) => x % 2 == 0), 
+).subscribe((x : number) => {
+    console.log(x);
+})
+
